af7f6b7e63089931e16c8f5b851cb6b8e47683c8
Kim-2sam2 <Kim-2sam2@github.com>
2019-05-02 14:33:10 +0900

    하단 네비게이션바 아이콘의 애니매이션 고정되도록 추가. (추후 협의 후 제거 가능).

--- app/src/main/java/com/impact/tripble/FixBottomIcon.java
+++ app/src/main/java/com/impact/tripble/FixBottomIcon.java
@@ -10,26 +10,27 @@ package com.impact.tripble;
 
 import android.annotation.SuppressLint;
 import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
 import android.util.Log;
 
 import java.lang.reflect.Field;
 
 class FixBottomIcon {
-    @SuppressLint("RestictedApi")
-    static void disableShiftMode(BottomNavigationView view){
-        BottomNavigationView barView = (BottomNavigationView) view.getChildAt(0);
-        try{
-            Field shiftingMode = barView.getClass().getDeclaredField("mShiftingMode");
+
+    @SuppressLint("RestrictedApi")
+    static void disableShiftMode(BottomNavigationView view) {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+        try {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
             shiftingMode.setAccessible(true);
-            shiftingMode.setBoolean(barView, false);
+            shiftingMode.setBoolean(menuView, false);
             shiftingMode.setAccessible(false);
 
-            for(int i=0; i<barView.getChildCount(); i++){
-                BottomNavigationItemView item = (BottomNavigationItemView) barView.getChildAt(i);
+            for (int i = 0; i < menuView.getChildCount(); i++) {
+                BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
 
                 item.setShiftingMode(false);
-
                 item.setChecked(item.getItemData().isChecked());
             }
         } catch (NoSuchFieldException e) {
--- app/src/main/java/com/impact/tripble/MainActivity.java
+++ app/src/main/java/com/impact/tripble/MainActivity.java
@@ -1,5 +1,6 @@
 package com.impact.tripble;
 
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigator();
+
+
+    }
+
+    protected void bottomNavigator(){
+
+        BottomNavigationView bottomNavigationView= findViewById(R.id.bottom_navigationBar);
+
+        FixBottomIcon.disableShiftMode(bottomNavigationView);
     }
 }
