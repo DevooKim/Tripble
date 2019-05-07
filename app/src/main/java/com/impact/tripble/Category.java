7c02ff23b4f3d92a2364a1058fa2a0cb400c03d2
Kim-2sam2 <Kim-2sam2@github.com>
2019-05-08 00:55:03 +0900

    make_id: 이름, next버튼 이외 구현 완료 Category: 하단 버튼제외 xml구현 완료

--- app/src/main/java/com/impact/tripble/Category.java
+++ app/src/main/java/com/impact/tripble/Category.java
@@ -1,15 +1,14 @@
 package com.impact.tripble;
 
-import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridLayout;
 
 import java.util.ArrayList;
-
 
 public class Category extends AppCompatActivity {
     GridLayout gl;
@@ -29,17 +28,17 @@ public class Category extends AppCompatActivity {
         setContentView(R.layout.category);
 
         setButton();
-        setClick();
-
     }
 
     private void setButton(){
-        list1 = new ArrayList();
-        gl = (GridLayout)findViewById(R.id.table);
+
+
+        //gl = (GridLayout)findViewById(R.id.table);
+        gl = new GridLayout(this);
 
         for(int i=1; i<6; i++){
-            tmp++;
-            for(int j=1; i<6; j++) {
+            for(int j=1; j<6; j++){
+                tmp++;
                 btn = new Button(this);
 
                 //xml에 구현해 놓은 View와 매칭//
@@ -47,51 +46,20 @@ public class Category extends AppCompatActivity {
                 int nameId = getResources().getIdentifier(name,"id",getPackageName());
                 btn = findViewById(nameId);
 
-                //각 View에 이미지넣기//
-                String str = "b"+(tmp*j);
+                //각 View에 이미지 넣기//
+                String str = "b"+tmp;
                 btn.setTag(str);
-                int resId = getResources().getIdentifier("bt_"+i+"x"+j,"drawable", getPackageName());
+                int resId = getResources().getIdentifier("bt_"+i+"x"+j,"drawable",getPackageName());
                 btn.setBackgroundResource(resId);
 
-                int path = getResources().getIdentifier("bt_y"+i+"x"+j,"drawable", getPackageName());
-                list2.add(path);
-                list1.add(btn);
-                gl.addView(btn);
+               ViewGroup gl = (ViewGroup)btn.getParent();
+               gl.removeView(btn);
+               gl.addView(btn);
+
             }
+
         }
-    }
 
-    public void setClick(){
-        for(i=0; i<25; i++){
-            bt_tmp = (Button) list1.get(i);
-            bt_tmp.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-//                    int id_y = (Integer) list2.get(i);
-//                    draw = getResources().getDrawable(id_y);
-//                    int id = getResources().getIdentifier("b"+ (i+1),"layout",getPackageName());
-//                    bt_check = (Button)findViewById(id);
-//
-//                    if(id_y != id && count < 5){
-//                        //String key = (String) v.getTag();   //흰배경의 아이콘
-//                        bt_tmp.setBackgroundResource(id_y);
-//                        count++;
-//                    } else if(id_y == id && count >0){
-//                        bt_tmp.setBackgroundResource(id);
-//                    }
-
-                    Drawable d1 = bt_tmp.getBackground();
-                    Drawable d2 = getResources().getDrawable((Integer)list2.get(i));
-
-                    if(d1.equals(d2)){
-                        bt_tmp.setBackgroundResource(((Integer)list2.get(i)));
-                    }else if(!(d1.equals(d2))){
-                        bt_tmp.setBackgroundResource(R.drawable.bt_1x1);
-                    }
-
-                }
-            });
-        }
     }
 
 }
