2caefe32af1082e5f8ffc39251f40550751162a1
Kim-2sam2 <Kim-2sam2@github.com>
2019-05-08 00:00:23 +0900

    make_id: 이름, next버튼 이외 구현 완료 Category 작업중

--- app/src/main/java/com/impact/tripble/Category.java
+++ app/src/main/java/com/impact/tripble/Category.java
@@ -1,13 +1,25 @@
 package com.impact.tripble;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
 
+import java.util.ArrayList;
+
+
 public class Category extends AppCompatActivity {
     GridLayout gl;
-    Button b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17,b18,b19,b20,b21,b22,b23,b24,b25;
+    Button btn, bt_tmp, bt_check;
+    ArrayList list1, list2;
+    String name;
+    Drawable draw;
+
+
+    int tmp=0, count=0, i;
 
 
 
@@ -15,40 +27,71 @@ public class Category extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.category);
+
+        setButton();
+        setClick();
+
     }
 
     private void setButton(){
-        b1=(Button)findViewById(R.id.oneX1);
-        b2=(Button)findViewById(R.id.oneX2);
-        b3=(Button)findViewById(R.id.oneX3);
-        b4=(Button)findViewById(R.id.oneX4);
-        b5=(Button)findViewById(R.id.oneX5);
-        b6=(Button)findViewById(R.id.twoX1);
-        b7=(Button)findViewById(R.id.twoX2);
-        b8=(Button)findViewById(R.id.twoX3);
-        b9=(Button)findViewById(R.id.twoX4);
-        b10=(Button)findViewById(R.id.twoX5);
-        b11=(Button)findViewById(R.id.threeX1);
-        b12=(Button)findViewById(R.id.threeX2);
-        b13=(Button)findViewById(R.id.threeX3);
-        b14=(Button)findViewById(R.id.threeX4);
-        b15=(Button)findViewById(R.id.threeX5);
-        b16=(Button)findViewById(R.id.fourX1);
-        b17=(Button)findViewById(R.id.fourX2);
-        b18=(Button)findViewById(R.id.fourX3);
-        b19=(Button)findViewById(R.id.fourX4);
-        b20=(Button)findViewById(R.id.fourX5);
-        b21=(Button)findViewById(R.id.fiveX1);
-        b22=(Button)findViewById(R.id.fiveX2);
-        b23=(Button)findViewById(R.id.fiveX3);
-        b24=(Button)findViewById(R.id.fiveX4);
-        b25=(Button)findViewById(R.id.fiveX5);
+        list1 = new ArrayList();
+        gl = (GridLayout)findViewById(R.id.table);
 
-        for(int i=1; i<26; i++){
-            String str = "b" + i;
+        for(int i=1; i<6; i++){
+            tmp++;
+            for(int j=1; i<6; j++) {
+                btn = new Button(this);
 
+                //xml에 구현해 놓은 View와 매칭//
+                name = "b"+tmp;
+                int nameId = getResources().getIdentifier(name,"id",getPackageName());
+                btn = findViewById(nameId);
+
+                //각 View에 이미지넣기//
+                String str = "b"+(tmp*j);
+                btn.setTag(str);
+                int resId = getResources().getIdentifier("bt_"+i+"x"+j,"drawable", getPackageName());
+                btn.setBackgroundResource(resId);
+
+                int path = getResources().getIdentifier("bt_y"+i+"x"+j,"drawable", getPackageName());
+                list2.add(path);
+                list1.add(btn);
+                gl.addView(btn);
+            }
         }
+    }
 
+    public void setClick(){
+        for(i=0; i<25; i++){
+            bt_tmp = (Button) list1.get(i);
+            bt_tmp.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+//                    int id_y = (Integer) list2.get(i);
+//                    draw = getResources().getDrawable(id_y);
+//                    int id = getResources().getIdentifier("b"+ (i+1),"layout",getPackageName());
+//                    bt_check = (Button)findViewById(id);
+//
+//                    if(id_y != id && count < 5){
+//                        //String key = (String) v.getTag();   //흰배경의 아이콘
+//                        bt_tmp.setBackgroundResource(id_y);
+//                        count++;
+//                    } else if(id_y == id && count >0){
+//                        bt_tmp.setBackgroundResource(id);
+//                    }
+
+                    Drawable d1 = bt_tmp.getBackground();
+                    Drawable d2 = getResources().getDrawable((Integer)list2.get(i));
+
+                    if(d1.equals(d2)){
+                        bt_tmp.setBackgroundResource(((Integer)list2.get(i)));
+                    }else if(!(d1.equals(d2))){
+                        bt_tmp.setBackgroundResource(R.drawable.bt_1x1);
+                    }
+
+                }
+            });
+        }
     }
 
 }
