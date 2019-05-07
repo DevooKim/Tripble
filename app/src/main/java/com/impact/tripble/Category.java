120b619ce94b97f1662294f72859d3b31dfe68ac
Kim-2sam2 <Kim-2sam2@github.com>
2019-05-08 01:06:27 +0900

    make_id: 이름, next버튼 이외 구현 완료 Category: 하단 버튼제외 xml구현 완료

--- app/src/main/java/com/impact/tripble/Category.java
+++ app/src/main/java/com/impact/tripble/Category.java
@@ -28,13 +28,15 @@ public class Category extends AppCompatActivity {
         setContentView(R.layout.category);
 
         setButton();
+        setClick();
     }
 
     private void setButton(){
 
-
         //gl = (GridLayout)findViewById(R.id.table);
         gl = new GridLayout(this);
+        list1 = new ArrayList();
+        list2 = new ArrayList();
 
         for(int i=1; i<6; i++){
             for(int j=1; j<6; j++){
@@ -56,10 +58,46 @@ public class Category extends AppCompatActivity {
                gl.removeView(btn);
                gl.addView(btn);
 
+               int path = getResources().getIdentifier("bt_y"+i+"x"+j,"drawable", getPackageName());
+               list2.add(path);
+               list1.add(btn);
+
             }
-
         }
+    }
 
+        public void setClick(){
+        for(i=0; i<24; i++){
+            bt_tmp = (Button) list1.get(i);
+            bt_tmp.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int id_y = (Integer) list2.get(i);
+                    //draw = getResources().getDrawable(id_y);
+                    int id = getResources().getIdentifier("b"+ i,"layout",getPackageName());
+                    bt_check = (Button)findViewById(id);
+
+                    if(id_y != id && count < 5){
+                        //String key = (String) v.getTag();   //흰배경의 아이콘
+                        bt_tmp.setBackgroundResource(id_y);
+                        count++;
+                    } else if(id_y == id && count >0){
+                        bt_tmp.setBackgroundResource(id);
+                    }
+
+                    Drawable d1 = bt_tmp.getBackground();
+                    Drawable d2 = getResources().getDrawable((Integer)list2.get(i));
+
+                    if(d1.equals(d2)){
+                        //bt_tmp.setBackgroundResource(((Integer)list2.get(i)));
+                        bt_tmp.setBackgroundResource(R.drawable.bt_1x1);
+                    }else if(!(d1.equals(d2))){
+                        bt_tmp.setBackgroundResource(R.drawable.bt_1x1);
+                    }
+
+                }
+            });
+        }
     }
 
 }
