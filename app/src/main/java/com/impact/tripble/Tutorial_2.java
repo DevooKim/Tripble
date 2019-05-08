4a53eb1ff43aa0c009ca718895371eed2af455da
dkzpd <mn03042@naver.com>
2019-05-08 19:10:47 +0900

    튜토리얼 구현 완료

--- app/src/main/java/com/impact/tripble/Tutorial_2.java
+++ app/src/main/java/com/impact/tripble/Tutorial_2.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class Tutorial_2 extends AppCompatActivity {
 
@@ -11,10 +12,16 @@ public class Tutorial_2 extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.introduce_2);
+
+        Button bt = (Button)findViewById(R.id.bt2);
+
+        bt.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(Tutorial_2.this, Tutorial_3.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
-    public void Next(View view){
-        Intent intent = new Intent(Tutorial_2.this, Tutorial_3.class);
-        startActivity(intent);
-    }
 }
--- app/src/main/java/com/impact/tripble/Tutorial_3.java
+++ app/src/main/java/com/impact/tripble/Tutorial_3.java
@@ -4,16 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class Tutorial_3 extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.introduce_3);
-    }
 
-    public void Start(View view){
-        Intent intent = new Intent(Tutorial_3.this, MainActivity.class);
-        startActivity(intent);
+        Button bt = (Button)findViewById(R.id.start);
+
+        bt.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(Tutorial_3.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
