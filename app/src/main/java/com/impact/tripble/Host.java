58ae1da0e005dc7815e56a9887c4b92dfbca06b9
kim-2sam2 <khwoo8026@naver.com>
2019-08-20 01:21:07 +0900

    setMission제외 오류 수정

--- app/src/main/java/com/impact/tripble/Host.java
+++ app/src/main/java/com/impact/tripble/Host.java
@@ -1,25 +1,43 @@
+//package com.impact.tripble;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class Host {
+//
+//    String host;
+//    String tel;
+//    List<Theme> mTheme;
+//
+//    public Host(){
+//    }
+//
+//    public Host(String title, String tel){
+//        this.title = title;
+//        this.tel = tel;
+//        this.mTheme = new ArrayList<Theme>();
+//    }
+//
+//    private void setTheme(Theme theme){
+//        mTheme.add(theme);
+//    }
+//
+//}
+
 package com.impact.tripble;
 
 import java.util.ArrayList;
 import java.util.List;
-
 public class Host {
 
     String title;
-    String tel;
-    List<Theme> mTheme;
+    String reward;
+    String position;
 
-    public Host(){
+    public Host(String title, String reward, String position){
+            this.title = title;
+            this.reward = reward;
+            this.position = position;
     }
-
-    public Host(String title, String tel){
-        this.title = title;
-        this.tel = tel;
-        this.mTheme = new ArrayList<Theme>();
-    }
-
-    private void setTheme(Theme theme){
-        mTheme.add(theme);
-    }
-
 }
+
